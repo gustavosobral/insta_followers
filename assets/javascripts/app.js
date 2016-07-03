1646d6710acaf5d6c736c38f0e33c173b385790a
Gustavo Sobral <ghsobral@gmail.com>
2016-07-03 00:46:36 -0300

    Add select all feature

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,1 +1,9 @@
 //= require_tree .
+
+function selectAll() {
+  checkboxes = document.getElementsByClassName('checkboxes');
+  for (var i = checkboxes.length - 1; i >= 0; i--) {
+    checkboxes[i].checked = true;
+  }
+  console.log('Clicked!');
+}
